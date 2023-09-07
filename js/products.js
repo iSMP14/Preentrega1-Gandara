da680845c80070539bef807fdd73438aef4158c7
iSMP <javiergcartes14@gmail.com>
2023-09-07 19:56:46 -0300

    Edicion basica en SCSS, html. añadidos nuevos arrays a script y adicion del archivo products

---
+++ js/products.js
@@ -0,0 +1,30 @@
+// Products Array
+const products = [
+    {
+        id: 1,
+        name: 'Fantasy Style Image',
+        priceUSD: 60,
+        image: 'fantasy_image.jpg',
+        style: 'Fantasy',
+    },
+    {
+        id: 2,
+        name: 'Anime Style Image',
+        priceUSD: 40,
+        image: 'anime_image.jpg',
+        style: 'Anime',
+    },
+    {
+        id: 3,
+        name: 'Realistic Style Image',
+        priceUSD: 50,
+        image: 'realistic_image.jpg',
+        style: 'Realistic',
+    }
+];
+
+// Arryas 
+
+const firstProduct = products[0];
+const secondProduct = products[1];
+const thirdProduct = products[2];
--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
 // Ecommerce IA Images //
 let totalCost = 0;
+let selectedProducts = []; // Array para almacenar productos seleccionados
 
-function addToTotal(cost) {
+function addToTotal(cost, product) {
   totalCost += cost;
-  console.log('Total so far: $' + totalCost);
+  selectedProducts.push(product); // Agregar el producto al array de productos seleccionados
+  console.log('Total: $' + totalCost);
 }
 
-let idProduct = parseInt(prompt('Lets buy an IA Image with us\n1-Realistic style\n2-Anime Style\n3-Fantasy style\n0-Exit/Pay'));
+let idProduct = parseInt(prompt('Let\'s buy an IA Image with us\n1-Realistic style\n2-Anime Style\n3-Fantasy style\n0-Exit/Pay'));
 
 while (idProduct !== 0) {
   switch (idProduct) {
     case 1:
       alert('You added Realistic style to your cart.');
-      addToTotal(50);
+      addToTotal(50, 'Realistic style');
       break;
     case 2:
       alert('You added Anime Style to your cart.');
-      addToTotal(40);
+      addToTotal(40, 'Anime Style');
       break;
     case 3:
       alert('You added Fantasy style to your cart.');
-      addToTotal(60);
+      addToTotal(60, 'Fantasy style');
       break;
     default:
       alert('Invalid product code.');
       break;
   }
 
-  idProduct = parseInt(prompt('Lets buy an IA Image with us\n1-Realistic style $50\n2-Anime Style $40\n3-Fantasy style $60\n0-Exit'));
+  idProduct = parseInt(prompt('Let\'s buy an IA Image with us\n1-Realistic style $50\n2-Anime Style $40\n3-Fantasy style $60\n0-Exit'));
 }
 
-alert('Your total purchase amount is: $' + totalCost);
+// Tabla de productos seleccionadoe y costo total
+console.log('Products selected and total cost:');
+console.table(selectedProducts);
+console.log('Total: $' + totalCost);
 
 if (totalCost !== 0) {
   let paymentMethod = parseInt(prompt('Select your payment method:\n1 - Credit Card\n2 - PayPal\n3 - Bitcoin'));
